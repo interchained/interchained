c4cb30017c900b89b1ae260fc9d340f5411683cd
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2026-03-02 16:57:46 -0500

    ITC core

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -12,7 +12,7 @@
  * for both interchainedd and interchained-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Interchained");
 
 
 #ifdef HAVE_BUILD_INFO
